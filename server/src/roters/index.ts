8f28dfef9944c22624bfb1a7044653d83f2ebed7
Elena Buraya <lenalatkovich@gmail.com>
2019-12-11 15:53:20 +0300

    new routers

--- server/src/roters/index.ts
+++ server/src/roters/index.ts
@@ -2,15 +2,21 @@ import Router from 'koa-router';
 import { Context } from 'koa';
 
 const router = new Router();
-  router.get('/', ctx => getIndex(ctx));
+  router.get('/', ctx => getLogin(ctx));
   router.get('/login', ctx => getLogin(ctx));
-
-  const getIndex = (ctx: Context) => {
-    ctx.body = 'This index';
-  }
+  router.get('/signup', ctx => getSignup(ctx));
+  router.get('/authOrganization', ctx => getGetKeyAuthOrganization(ctx));
 
   const getLogin = (ctx: Context) => {
     ctx.body = 'This login';
+  }
+
+  const getSignup = (ctx: Context) => {
+    ctx.body = 'This signup';
+  }
+
+  const getGetKeyAuthOrganization = (ctx: Context) => {
+    ctx.status = 200;
   }
 
   export default router;
