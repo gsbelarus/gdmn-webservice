413e0f184e0b0d5c238ba4df8b9af7d5aa645fc0
Elena Buraya <lenalatkovich@gmail.com>
2019-12-16 13:23:26 +0300

    server: wip

--- server/src/roters/index.ts
+++ server/src/roters/index.ts
@@ -6,7 +6,9 @@ import { User } from '../models';
 const router = new Router();
 router.get('/', async ctx => await getLogin(ctx));
 router.post('/login', async ctx => getLogin(ctx));
+router.get('/signout', async ctx => getSignOut(ctx));
 router.get('/signup', ctx => getSignup(ctx));
+router.get('/me', ctx => getMe(ctx));
 router.get('/authOrganization', ctx => getGetKeyAuthOrganization(ctx));
 
 const getLogin = async (ctx: any) => {
@@ -16,6 +18,27 @@ const getLogin = async (ctx: any) => {
   ctx.login(user);
   ctx.status = 200;
   ctx.body = JSON.stringify({ status: 200, result: user ? user.id : false});
+  return user;
+}
+
+const getMe = async (ctx: any) => {
+  if(ctx.isAuthenticated()) {
+    ctx.status = 200;
+    ctx.body = JSON.stringify({ status: 200, result: ctx.passport.user});
+  } else {
+    ctx.status = 200;
+    ctx.body = JSON.stringify({ status: 200, result: 'not authenticated'});
+  }
+}
+
+const getSignOut = (ctx: any) => {
+  if(ctx.isAuthenticated()) {
+    ctx.status = 200;
+    ctx.body = JSON.stringify({ status: 200, result: 'sign out successful'});
+  } else {
+    ctx.status = 200;
+    ctx.body = JSON.stringify({ status: 200, result: 'вышел раньше или не заходил'});
+  }
 }
 
 const getSignup = (ctx: any) => {
