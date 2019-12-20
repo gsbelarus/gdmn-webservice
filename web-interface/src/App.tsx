b308eb501e0551dc985b7cbaaf41408f88a5bec8
andreik <gs1994@gmail.com>
2019-12-20 12:57:48 +0300

    revert to initial version

--- web-interface/src/App.tsx
+++ web-interface/src/App.tsx
@@ -1,43 +1,11 @@
 import React from 'react';
-import { useLogin } from './useLogin';
-import { DefaultButton, Stack } from "office-ui-fabric-react";
-import { LoginPage } from './LoginPage';
-import { OurList } from './OurList';
 
 const App: React.FC = () => {
-
-  const [login, doLogin, doLogout] = useLogin();
-
   return (
     <div>
-      {login.loginState}
-      {
-        login.loginState === 'LOGGED_OUT'
-        ?
-          <LoginPage
-            onLogin={
-              async (user, password) => {
-                const loginData = await doLogin(user, password);
-                window.alert(loginData.loginState);
-              }
-            }
-          />
-        : login.loginState === 'LOGGED_IN'
-        ?
-          <Stack horizontal>
-            <DefaultButton
-              text="Logout"
-              onClick={doLogout}
-            />
-          </Stack>
-        :
-          null
-      }
-      <div>
-        <OurList loginState={login.loginState} />
-      </div>
+      Hello!
     </div>
   );
-}
+};
 
 export default App;
