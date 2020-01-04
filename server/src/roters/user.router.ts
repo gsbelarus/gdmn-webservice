53684e04b12320f0b93eacdba3a47e1b60f67144
Elena Buraya <lenalatkovich@gmail.com>
2020-01-04 10:20:04 +0300

    server: wip

--- server/src/roters/user.router.ts
+++ server/src/roters/user.router.ts
@@ -14,6 +14,7 @@ router.get('/byDevice', ctx => getUsersByDevice(ctx));
 router.get('/all', ctx => getUsers(ctx));
 router.get('/byOrganisation', ctx => getUsersByOrganisation(ctx));
 router.post('/edite', ctx => editeProfile(ctx));
+router.post('/addOrganisation', ctx => addOrganisation(ctx));
 
 const getUsersByDevice = async (ctx: any) => {
   if(ctx.isAuthenticated()) {
