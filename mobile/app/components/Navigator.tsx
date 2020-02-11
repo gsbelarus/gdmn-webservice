d1a1703710160148dc41782e6004504bd1298415
ShiralievaSamira <sam.shiraliyeva@mail.ru>
2020-02-11 16:19:41 +0300

    add home navigation

--- mobile/app/components/Navigator.tsx
+++ mobile/app/components/Navigator.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ActivationPage from './ActivationPage';
 import MainPage from './MainPage';
 import MessagePage from './MessagePage';
@@ -7,6 +8,7 @@ import DocumentPage from './DocumentPage';
 import DocumentFilterPage from './DocumentFilterPage';
 import DirectoryPage from './DirectoryPage';
 import ProductPage from './ProductPage';
+import { TouchableOpacity } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { AntDesign } from '@expo/vector-icons';
@@ -41,23 +43,67 @@ const AppNavigator = createStackNavigator(
         },
         SendMessagePage: {
             screen: SendMessagePage,
-            navigationOptions: {  }
+            navigationOptions: { }
         },
         DocumentPage: {
             screen: DocumentPage,
-            navigationOptions: {  }
+            navigationOptions: ({navigation}) => ({
+                headerRight: (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('MainPage')}
+                        style={{height: 50, width: 50, justifyContent: 'center'}}>
+                            <AntDesign
+                                size={25}
+                                color='#FFF' 
+                                name='home' 
+                            />
+                    </TouchableOpacity>
+            )}  )
         },
         DocumentFilterPage: {
             screen: DocumentFilterPage,
-            navigationOptions: {  }
+            navigationOptions: ({navigation}) => ({
+                headerRight: (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('MainPage')}
+                        style={{height: 50, width: 50, justifyContent: 'center'}}>
+                            <AntDesign
+                                size={25}
+                                color='#FFF' 
+                                name='home' 
+                            />
+                    </TouchableOpacity>
+            )}  )
         },
         DirectoryPage: {
             screen: DirectoryPage,
-            navigationOptions: {  }
+            navigationOptions: ({navigation}) => ({
+                headerRight: (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('MainPage')}
+                        style={{height: 50, width: 50, justifyContent: 'center'}}>
+                            <AntDesign
+                                size={25}
+                                color='#FFF' 
+                                name='home' 
+                            />
+                    </TouchableOpacity>
+            )}  )
         },
         ProductPage: {
             screen: ProductPage,
-            navigationOptions: {  }
+            navigationOptions: ({navigation}) => ({
+                headerRight: (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('MainPage')}
+                        style={{height: 50, width: 50, justifyContent: 'center'}}>
+                            <AntDesign
+                                size={25}
+                                color='#FFF' 
+                                name='home' 
+                            />
+                    </TouchableOpacity>
+            )}  )
         }
     },
     {
