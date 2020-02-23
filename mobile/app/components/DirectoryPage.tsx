ebb42e1a6109c51fb5acfbba523e5b4a7d04bc44
Stanislau Shliakhtsich <stasgm@gmail.com>
2020-02-24 01:01:51 +0300

    style: prettify

--- mobile/app/components/DirectoryPage.tsx
+++ mobile/app/components/DirectoryPage.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, StatusBar, Text, AsyncStorage, ScrollView} from 'react-native';
 
@@ -16,7 +17,7 @@ const DirectoryPage = (): JSX.Element => {
     <View style={styles.container}>
       <ScrollView style={{flex: 1}}>
         {
-          !data ? <Text>Справочник пуст</Text> : data.map( (item, idx) => <View style={styles.productView} key={idx}>
+          !data ? <Text>Справочник пуст</Text> : data.map((item, idx) => <View style={styles.productView} key={idx}>
             <View style={styles.productTextView}>
               <View style={styles.productIdView}>
                 <Text style={styles.productId}>{idx + 1}</Text>
