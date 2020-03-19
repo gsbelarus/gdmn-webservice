03b822b0a12ed4322c59da84ed25a4558cb3e55c
Elena Buraya <lenalatkovich@gmail.com>
2020-03-19 16:54:39 +0300

    feat: data and them interface

---
+++ mobile/src/model/inventory.ts
@@ -0,0 +1,54 @@
+
+export interface IDocument {
+	id: number;
+	head: IHead;
+	lines: ILine[];
+ }
+ 
+ export interface IHead {
+	doctype: number;
+	fromcontactId: number;
+	tocontactId: number;
+	date: string;
+	status: number;
+ }
+ 
+ export interface ILine {
+	id: string;
+	goodId: number;
+	quantity: number;
+ }
+ 
+ export interface IUser {
+	id: string;
+	userName: string;
+	companies?: string[];
+	firstName?: string;
+	lastName?: string;
+	phoneNumber?: string;
+ }
+
+ export interface IRemain {
+	goodId: number;
+	quantity: number;
+	price: number;
+	contactId: number;
+ }
+
+ export interface IContact {
+	id: number;
+	name: string;
+	type: number;
+ }
+
+ export interface IDocumentType {
+	id: number;
+	name: string;
+ }
+
+ export interface IGood {
+	id: number;
+	name: string;
+	barcode: string;
+	alias: string;
+ }
