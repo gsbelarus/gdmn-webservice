01067409b416c969594708640188665c861d560c
Elena Buraya <lenalatkovich@gmail.com>
2020-04-08 15:31:32 +0300

    fix: small change

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -8,7 +8,7 @@ router.post('/signup', signUp);
 router.post('/login', logIn);
 router.get('/logout', authMiddleware, logOut);
 router.get('/user', authMiddleware, getCurrentUser);
-router.get('/DEVICE/CODE', getActivationCode);
-router.post('/DEVICE/CODE', verifyCode);
+router.get('/device/code', getActivationCode);
+router.post('/device/code', verifyCode);
 
 export default router;
