141b7db8871b06539b38a5e5ec3b046c92924a9d
Elena Buraya <lenalatkovich@gmail.com>
2020-04-08 17:40:14 +0300

    fix: change route for companies

--- server/src/routes/company.router.ts
+++ server/src/routes/company.router.ts
@@ -1,11 +1,11 @@
 import Router from 'koa-router';
-import { getCompaniesByUser, getCompanyProfile, addCompany, editCompanyProfile } from '../controllers/company';
+import { getCompanyProfile, addCompany, editCompanyProfile, getUsersByCompany } from '../controllers/company';
 import { authMiddleware } from '../middleware/authRequired';
 
 const router = new Router({ prefix: '/company' });
 
 router.post('/', authMiddleware, addCompany);
-router.get('/user/:id', authMiddleware, getCompaniesByUser);
 router.get('/:id', authMiddleware, getCompanyProfile);
 router.put('/:id', authMiddleware, editCompanyProfile);
+router.put('/:id/users', authMiddleware, getUsersByCompany);
 export default router;
