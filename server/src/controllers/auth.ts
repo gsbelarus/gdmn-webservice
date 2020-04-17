a09f9a87c86649d5019d35e3bdc109a77685a45e
Elena Buraya <lenalatkovich@gmail.com>
2020-04-17 15:50:40 +0300

    fix: change response in auth

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -13,7 +13,7 @@ const logIn = async (ctx: ParameterizedContext, next: Next): Promise<void> => {
 
   if (!user) {
     log.info('failed login attempt');
-    const res: IResponse<string> = { status: 404, result: 'не верный пользователь и\\или пароль ' };
+    const res: IResponse<string> = { result: false, error: 'не верный пользователь и\\или пароль ' };
     ctx.throw(404, JSON.stringify(res));
   }
 
@@ -24,8 +24,8 @@ const logIn = async (ctx: ParameterizedContext, next: Next): Promise<void> => {
 
     log.info(`user ${user} successfully logged in`);
 
-    const res: IResponse<boolean | IUser> = { status: 200, result: user || false };
-
+    const res: IResponse<IUser> = { result: true, data: user };
+    ctx.status = 200;
     ctx.body = JSON.stringify(res);
   }
 };
@@ -38,8 +38,8 @@ const getCurrentUser = (ctx: ParameterizedContext): void => {
 
   delete user.password;
 
-  const res: IResponse<IUser> = { status: 200, result: user };
-
+  const res: IResponse<IUser> = { result: true, data: user };
+  ctx.status = 200;
   ctx.body = JSON.stringify(res);
 };
 
@@ -49,7 +49,8 @@ const logOut = (ctx: ParameterizedContext): void => {
 
   ctx.logout();
 
-  const res: IResponse<string> = { status: 200, result: 'successfully logged out' };
+  const res: IResponse<string> = { result: true, data: 'successfully logged out' };
+  ctx.status = 200;
   ctx.body = JSON.stringify(res);
 };
 
@@ -75,12 +76,14 @@ const signUp = async (ctx: ParameterizedContext): Promise<void> => {
       ),
     );
 
-    ctx.body = JSON.stringify({ status: 201, result: newUser });
+    const res = { result: true, data: newUser };
+    ctx.status = 201;
+    ctx.body = JSON.stringify(res);
     log.info('signed up successful');
   } else {
     log.info('a user already exists');
-    const res: IResponse<string> = { status: 404, result: 'a user already exists' };
-    ctx.throw(404, JSON.stringify(res));
+    const res: IResponse<string> = { result: false, error: 'a user already exists' };
+    ctx.throw(400, JSON.stringify(res));
   }
 };
 
@@ -88,6 +91,7 @@ const verifyCode = async (ctx: ParameterizedContext): Promise<void> => {
   const data: IActivationCode[] | undefined = await readFile(PATH_LOCAL_DB_ACTIVATION_CODES);
   const code = data && data.find(el => el.code === ctx.query.code);
   let result: IResponse<string>;
+  let status: number;
 
   if (code) {
     const date = new Date(code.date);
@@ -95,23 +99,29 @@ const verifyCode = async (ctx: ParameterizedContext): Promise<void> => {
 
     if (date >= new Date()) {
       await writeFile(PATH_LOCAL_DB_ACTIVATION_CODES, JSON.stringify(data?.filter(el => el.code !== ctx.query.code)));
-      result = { status: 200, result: code.user };
+      status = 200;
+      result = { result: true, data: code.user };
       log.info('device has been successfully activated');
     } else {
-      result = { status: 404, result: 'invalid activation code' };
+      status = 404;
+      result = { result: false, error: 'invalid activation code' };
       log.warn('invalid activation code');
     }
   } else {
-    result = { status: 404, result: 'invalid activation code' };
+    status = 404;
+    result = { result: false, error: 'invalid activation code' };
     log.warn('invalid activation code');
   }
+  ctx.status = status;
   ctx.body = JSON.stringify(result);
 };
 
 const getActivationCode = async (ctx: ParameterizedContext): Promise<void> => {
   const userId = ctx.query.user;
   const code = await saveActivationCode(userId);
-  ctx.body = JSON.stringify({ status: 200, result: code });
+  ctx.status = 200;
+  const result: IResponse<string> = { result: true, data: code };
+  ctx.body = JSON.stringify(result);
   log.info('activation code generated successfully');
 };
 
