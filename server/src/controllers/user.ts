d031c59c8dcd15d1b98a2de58c3f388e3bf7435d
damned_god <stasgm@gmail.com>
2020-07-01 14:03:47 +0300

    feat: fix user-controller

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,8 +1,7 @@
 import { ParameterizedContext } from 'koa';
 import log from '../utils/logger';
-import { IResponse, IDeviceState, IDevice, IActivationCode, IUser, IUserProfile } from '../../../common';
+import { IResponse, IDeviceState, IUser, IUserProfile } from '../../../common';
 import { userService } from '../services';
-import { users, devices, codes } from '../services/dao/db';
 
 export const makeProfile = (user: IUser) => ({
   id: user.id,
@@ -67,90 +66,60 @@ const updateUser = async (ctx: ParameterizedContext): Promise<void> => {
   }
 
   try {
-    const userId = await userService.updateOne(user);
+    const id = await userService.updateOne({ ...user, id: userId });
 
-    const result: IResponse<string> = { result: true, data: userId };
+    const result: IResponse<string> = { result: true, data: id };
 
     ctx.status = 200;
     ctx.body = result;
 
-    log.warn('updateUser: OK');
+    log.info('updateUser: OK');
+  } catch (err) {
+    ctx.throw(400, err);
+  }
+};
+
+const removeUser = async (ctx: ParameterizedContext): Promise<void> => {
+  const { id: userId } = ctx.params;
+
+  if (!userId) {
+    ctx.throw(400, 'не указан идентификатор пользователя');
+  }
+
+  // TODO
+  try {
+    await userService.deleteOne(userId);
+
+    const result: IResponse<void> = { result: true };
+
+    ctx.status = 200;
+    ctx.body = result; //TODO передавать только код 204 без body
+
+    log.info('removeUser: OK');
   } catch (err) {
     ctx.throw(400, err);
   }
 };
 
 const getDevicesByUser = async (ctx: ParameterizedContext): Promise<void> => {
-  const userId: string = ctx.params.id;
-  const result: IResponse<IDeviceState[]> = {
-    result: true,
-    data: (await devices.read())
-      .filter(device => device.user === userId)
-      .map(device => ({ uid: device.uid, state: device.blocked ? 'blocked' : 'active' })),
-  };
-  log.info('get devices by user successfully');
-  ctx.type = 'application/json';
-  ctx.status = 200;
-  ctx.body = JSON.stringify(result);
-};
+  const { id: userId } = ctx.params;
 
-const removeUser = async (ctx: ParameterizedContext): Promise<void> => {
-  const { id } = ctx.params;
-  const user = await users.find(el => id !== el.id);
-
-  if (!user) {
-    log.warn(`removeUser: user ${id} not found`);
-    const res: IResponse<string> = { result: false, error: 'пользователь не найден' };
-    ctx.throw(422, JSON.stringify(res));
+  if (!userId) {
+    ctx.throw(400, 'не указан идентификатор пользователя');
   }
 
-  await users.delete(id);
+  try {
+    const devices = await userService.findDevicesByUserId(userId);
 
-  // (await codes.read()).filter(el => el.user === id).forEach(async i => await codes.delete(i.id));
-  // (await devices.read()).filter(el => el.user === id).forEach(i => devices.delete(i.uid));
+    const result: IResponse<IDeviceState[]> = { result: true, data: devices };
 
-  ctx.type = 'application/json';
-  ctx.status = 204;
-  log.info('users removed successfully');
+    ctx.status = 200;
+    ctx.body = result;
+
+    log.info('getDevicesByUser: OK');
+  } catch (err) {
+    ctx.throw(400, err);
+  }
 };
 
-const addCompanyToUser = async (userId: string, companies: string[]) => {
-  // const allUsers: IUser[] | undefined = await readFile(PATH_LOCAL_DB_USERS);
-  // const user = allUsers?.filter(item => item.id === userId)[0];
-  // if (!(user && allUsers)) return;
-  // const idx = allUsers.findIndex(item => item.userName === user.userName);
-  // if (!allUsers || idx === undefined || idx < 0) return 1;
-  // await writeFile({
-  //   filename: PATH_LOCAL_DB_USERS,
-  //   data: JSON.stringify([
-  //     ...allUsers.slice(0, idx),
-  //     { ...user, companies: user && user.companies ? [...user?.companies, ...companies] : companies },
-  //     ...allUsers.slice(idx + 1),
-  //   ]),
-  // });
-  // return 1;
-};
-
-// const removeUsersFromCompany = async (ctx: ParameterizedContext): Promise<void> => {
-//   if (ctx.isAuthenticated()) {
-//     const companyId: string = ctx.params.id;
-//     const users: string[] = ctx.request.body?.users;
-//     const allUsers: IUser[] | undefined = await readFile(PATH_LOCAL_DB_USERS);
-//     const newUsers = allUsers?.map(el =>
-//       users.findIndex((u: string) => u === el.id) !== -1
-//         ? { ...el, companies: el.companies?.filter(o => o !== companyId) }
-//         : el,
-//     );
-//     /** Пока только удалим организацию у пользователей */
-//     await writeFile(PATH_LOCAL_DB_USERS, JSON.stringify(newUsers));
-
-//     ctx.body = JSON.stringify({ status: 200, result: 'users removed successfully' });
-//     log.info('users removed successfully');
-//   } else {
-//     ctx.status = 403;
-//     ctx.body = JSON.stringify({ status: 403, result: 'access denied' });
-//     log.warn('access denied');
-//   }
-// };
-
-export { getDevicesByUser, getUsers, getUser, removeUser, updateUser, addCompanyToUser };
+export { getDevicesByUser, getUsers, getUser, removeUser, updateUser };
--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,4 +1,4 @@
-import { users } from './dao/db';
+import { users, devices } from './dao/db';
 import { makeProfile } from '../controllers/user';
 import { IUser } from '../../../common';
 
@@ -16,6 +16,11 @@ const findAll = async () => (await users.read()).map(el => makeProfile(el));
 const updateOne = async (user: IUser) => {
   const oldUser = await users.find(user.id || user.userName);
 
+  // Удаляем поля которые нельзя перезаписывать
+  delete user.password;
+
+  // Проверяем свойство 'companies' => Проверяем что организации существуют
+
   if (!oldUser) {
     throw new Error('пользователь не найден');
   }
@@ -25,13 +30,61 @@ const updateOne = async (user: IUser) => {
   return user.id;
 };
 
+/**
+ * Удаляет одного пользователя
+ * @param {string} id - идентификатор пользователя
+ * */
+const deleteOne = async (id: string): Promise<void> => {
+  if (!(await users.find(id))) {
+    throw new Error('user not found');
+  }
+
+  // TODO Если пользователь являестя админом организации то прерывать
+  // удаление с соответствующим сообщением
+  await users.delete(id);
+};
+
+/**
+ * Возвращает списолк устройст пользователя
+ * @param {string} id - идентификатор пользователя
+ * */
+const findDevicesByUserId = async (userId: string) => {
+  const user = await users.find(userId);
+
+  if (!user) {
+    throw new Error('пользователь не найден');
+  }
+
+  return (await devices.read())
+    .filter(device => device.user === userId)
+    .map(device => ({ uid: device.uid, state: device.blocked ? 'blocked' : 'active' }));
+};
+
 const addCompanyToUser = async (userId: string, companyName: string) => {
   const user = await findOne(userId);
   if (user.companies?.some(i => companyName === i)) {
-    throw new Error('организация уже существует у пользователя');
+    throw new Error('организация уже привязана к пользователю');
   }
 
   return users.update({ ...user, companies: [...user.companies, companyName] });
 };
 
-export { findOne, findByUserName, updateOne, addCompanyToUser, findAll };
+const removeCompanyFromUser = async (userId: string, companyName: string) => {
+  const user = await findOne(userId);
+  if (user.companies?.some(i => companyName === i)) {
+    throw new Error('организация не привязана к пользователю');
+  }
+
+  return users.update({ ...user, companies: user.companies?.filter(i => i === companyName) });
+};
+
+export {
+  findOne,
+  findAll,
+  findByUserName,
+  updateOne,
+  deleteOne,
+  addCompanyToUser,
+  removeCompanyFromUser,
+  findDevicesByUserId,
+};
