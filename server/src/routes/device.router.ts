87953f7be877c8c5277d592970a8783dc7d7c388
Elena Buraya <lenalatkovich@gmail.com>
2020-07-07 20:21:12 +0300

    fix: route get device

--- server/src/routes/device.router.ts
+++ server/src/routes/device.router.ts
@@ -16,7 +16,7 @@ const router = new Router({ prefix: '/devices' });
 
 router.post('/', compose([deviceMiddleware, authMiddleware]), addDevice);
 router.get('/', compose([deviceMiddleware, authMiddleware]), getDevices);
-router.get('/:id', getDevice);
+router.get('/:id/user/:userId', getDevice);
 router.get('/:id/currentuser', compose([deviceMiddleware, authMiddleware]), getDeviceByCurrentUser);
 router.get('/:id/users', compose([deviceMiddleware, authMiddleware]), getUsersByDevice);
 router.patch('/:id/user/:userId', compose([deviceMiddleware, authMiddleware]), updateDevice);
