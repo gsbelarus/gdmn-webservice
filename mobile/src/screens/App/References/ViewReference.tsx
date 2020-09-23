1add884c2dee0ed8c723ce76341325d26417d409
Elena Buraya <lenalatkovich@gmail.com>
2020-09-23 17:33:12 +0300

    fix: filter reference

--- mobile/src/screens/App/References/ViewReference.tsx
+++ mobile/src/screens/App/References/ViewReference.tsx
@@ -1,12 +1,14 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useScrollToTop, useTheme, useNavigation } from '@react-navigation/native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { Text, Searchbar } from 'react-native-paper';
+import { View, FlatList, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import { Text, Searchbar, Button, IconButton } from 'react-native-paper';
 
 import { IReference } from '../../../../../common';
 import ItemSeparator from '../../../components/ItemSeparator';
 import SubTitle from '../../../components/SubTitle';
+import styles from '../../../styles/global';
 
 interface IField {
   id: number;
@@ -41,6 +43,9 @@ const ViewReferenceScreen = ({ route }) => {
 
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredList, setFilteredList] = useState<IReference>();
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [doScanned, setDoScanned] = useState(false);
 
   const { item: refItem }: { item: IReference } = route.params;
 
@@ -61,26 +66,105 @@ const ViewReferenceScreen = ({ route }) => {
 
   const renderItem = ({ item }: { item: IField }) => <LineItem item={item} />;
 
+  useEffect(() => {
+    const permission = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+    permission();
+  }, []);
+
+  const handleBarCodeScanned = (data: string) => {
+    setScanned(true);
+    Alert.alert('Сохранить результат?', data, [
+      {
+        text: 'Да',
+        onPress: () => {
+          setDoScanned(false);
+          setSearchQuery(data);
+          setScanned(false);
+        },
+      },
+      {
+        text: 'Нет',
+        onPress: () => {
+          // setDoScanned(false);
+          // onChangeText(data);
+          setScanned(false);
+        },
+      },
+    ]);
+  };
+
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={[localStyles.content, { backgroundColor: colors.card }]}>
-        <SubTitle styles={[localStyles.title, { backgroundColor: colors.background }]}>{filteredList?.name}</SubTitle>
-        <ItemSeparator />
-        <Searchbar
-          placeholder="Поиск"
-          onChangeText={setSearchQuery}
-          value={searchQuery}
-          style={localStyles.searchBar}
-        />
-        <ItemSeparator />
-        <FlatList
-          ref={ref}
-          data={filteredList?.data}
-          keyExtractor={(_, i) => String(i)}
-          renderItem={renderItem}
-          ItemSeparatorComponent={ItemSeparator}
-        />
-      </View>
+      <>
+        {hasPermission === null ? (
+          <Text style={styles.title}>Запрос на получение доступа к камере</Text>
+        ) : hasPermission === false ? (
+          <Text style={styles.title}>Нет доступа к камере</Text>
+        ) : undefined}
+        {doScanned ? (
+          <>
+            <BarCodeScanner
+              onBarCodeScanned={({ _, data }) => (scanned ? undefined : handleBarCodeScanned(data))}
+              style={StyleSheet.absoluteFillObject}
+            />
+            <Button
+              onPress={() => {
+                setScanned(false);
+                setDoScanned(false);
+              }}
+            >
+              Назад
+            </Button>
+          </>
+        ) : (
+          <View style={[localStyles.content, { backgroundColor: colors.card }]}>
+            <SubTitle styles={[localStyles.title, { backgroundColor: colors.background }]}>
+              {filteredList?.name}
+            </SubTitle>
+            <ItemSeparator />
+            <View style={localStyles.flexDirectionRow}>
+              <Searchbar
+                placeholder="Поиск"
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+                style={
+                  filteredList && filteredList.type === 'weighedgoods'
+                    ? [localStyles.flexGrow, localStyles.searchBar]
+                    : localStyles.searchBar
+                }
+              />
+              {filteredList && filteredList.type === 'weighedgoods' ? (
+                <IconButton
+                  icon="barcode-scan"
+                  size={26}
+                  style={localStyles.iconSettings}
+                  onPress={() => setDoScanned(true)}
+                />
+              ) : undefined}
+            </View>
+            <ItemSeparator />
+            <FlatList
+              ref={ref}
+              data={
+                filteredList && filteredList.type === 'weighedgoods'
+                  ? filteredList?.data.filter((item) =>
+                      searchQuery.length === 13 && !Number.isNaN(searchQuery)
+                        ? item.id.toString().includes(Number(searchQuery).toString().slice(0, -1)) ||
+                          item.id.toString().includes(Number(searchQuery).toString())
+                        : item.id.toString().includes(Number(searchQuery).toString()),
+                    )
+                  : filteredList?.data
+              }
+              keyExtractor={(_, i) => String(i)}
+              renderItem={renderItem}
+              ItemSeparatorComponent={ItemSeparator}
+            />
+          </View>
+        )}
+      </>
     </TouchableWithoutFeedback>
   );
 };
@@ -101,6 +185,15 @@ const localStyles = StyleSheet.create({
   },
   details: {
     margin: 10,
+  },
+  flexDirectionRow: {
+    flexDirection: 'row',
+  },
+  flexGrow: {
+    flexGrow: 10,
+  },
+  iconSettings: {
+    width: 36,
   },
   item: {
     alignItems: 'center',
