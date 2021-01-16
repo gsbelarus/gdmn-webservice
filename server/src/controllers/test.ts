43a2dbe8e88fb76f7bbcfeb19d806ae844241ebd
Stanislau Shliakhtsich <stasgm@gmail.com>
2021-01-16 12:04:35 +0300

    feat: server fix

--- server/src/controllers/test.ts
+++ server/src/controllers/test.ts
@@ -1,14 +1,12 @@
-import { readFile } from '../utils/workWithFile';
-import dev from '../../config/dev';
 import { ParameterizedContext } from 'koa';
 import log from '../utils/logger';
 import { IGood, IContact, IDocumentType, IDocument, IRemain, IResponse } from '../../../common';
 
-const PATH_GOODS = `${dev.FILES_PATH}\\Goods.json`;
-const PATH_DOCUMENT_TYPE = `${dev.FILES_PATH}\\GD_DocumentType.json`;
-const PATH_CONTACT = `${dev.FILES_PATH}\\GD_Contact.json`;
-const PATH_DOCUMENT = `${dev.FILES_PATH}\\Document.json`;
-const PATH_REMAINS = `${dev.FILES_PATH}\\Remains.json`;
+// const PATH_GOODS = `${dev.FILES_PATH}\\Goods.json`;
+// const PATH_DOCUMENT_TYPE = `${dev.FILES_PATH}\\GD_DocumentType.json`;
+// const PATH_CONTACT = `${dev.FILES_PATH}\\GD_Contact.json`;
+// const PATH_DOCUMENT = `${dev.FILES_PATH}\\Document.json`;
+// const PATH_REMAINS = `${dev.FILES_PATH}\\Remains.json`;
 
 interface IAllData {
   goods?: IGood[];
@@ -18,13 +16,14 @@ interface IAllData {
   docs?: IDocument[];
 }
 
-const getAllData = async (ctx: ParameterizedContext): Promise<void> => {
-  const goods: IGood[] | undefined = await readFile(PATH_GOODS);
-  const documenttypes: IDocumentType[] | undefined = await readFile(PATH_DOCUMENT_TYPE);
-  const contacts: IContact[] | undefined = await readFile(PATH_CONTACT);
-  const docs: IDocument[] | undefined = await readFile(PATH_DOCUMENT);
-  const remains: IRemain[] | undefined = await readFile(PATH_REMAINS);
-  const result: IResponse<IAllData> = { result: true, data: { goods, remains, documenttypes, contacts, docs } };
+const getAllData = (ctx: ParameterizedContext) => {
+  // const goods: IGood[] | undefined = await readFile(PATH_GOODS);
+  // const documenttypes: IDocumentType[] | undefined = await readFile(PATH_DOCUMENT_TYPE);
+  // const contacts: IContact[] | undefined = await readFile(PATH_CONTACT);
+  // const docs: IDocument[] | undefined = await readFile(PATH_DOCUMENT);
+  // const remains: IRemain[] | undefined = await readFile(PATH_REMAINS);
+  // const result: IResponse<IAllData> = { result: true, data: { goods, remains, documenttypes, contacts, docs } };
+  const result: IResponse<IAllData> = { result: true, data: undefined };
   ctx.status = 200;
   ctx.body = JSON.stringify(result);
   log.info('get all data');
